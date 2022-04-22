8ccaa110ae04431a754268ad462438e61f128c0e
Alina_Borovskih <alinaberseneva@icloud.com>
2022-04-22 20:40:44 +0300

    Add task 013

---
+++ Task013/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("Введите число: ");
+int number = int.Parse(Console.ReadLine());
+
+if (number >= 1000)
+{
+    number = number / 100 % 10;
+Console.WriteLine($"Вывести третью цифру числа {number} ");
+}
+else if (number < 100)
+{
+    Console.WriteLine($"Третьей цифры заданного числа нет ");
+}
+ else if (number < 1000)
+{
+    number = number % 10;
+    Console.WriteLine($"Вывести третью цифру числа {number} ");
+}  
