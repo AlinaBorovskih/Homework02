ef6eaa0a556ab73925f0a8143d61b3278053738a
Alina_Borovskih <alinaberseneva@icloud.com>
2022-04-22 20:49:45 +0300

    Add task 015

---
+++ Task015/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите цифру от 1 до 7");
+int day = int.Parse(Console.ReadLine());
+
+if (day >= 6)
+{
+    Console.WriteLine("Этот день выходной");
+} 
+else if (day <= 5)
+{
+    Console.WriteLine("Этот день не выходной");
+} 
